ff9486f8e51eb6cb09268f11c9e79482558cd47b
Jhonatan Adrian <jhona.pp.29@gmail.com>
2024-09-13 22:28:05 -0500

    primer commit, creacion del proyecto

---
+++ src/test/java/com/pioriko/ms_restaurante/MsRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pioriko.ms_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
